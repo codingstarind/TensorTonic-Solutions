6ad64bd31a546836e2eb22fca53fb345b9f23d44
Mehta Soham Priyadarshi <be24b030@smail.iitm.ac.in>
2026-05-29 11:05:50 +0530

    Add solution for la-solve-linear-system

---
+++ study-plans/math-linear-algebra/la-solve-linear-system/la-solve-linear-system.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def solve_linear_system(A, b):
+    """
+    Returns: float64 array, the solution x to A @ x = b.
+    """
+    A = np.array(A)
+    b= np.array(b)
+    
+    x = np.linalg.inv(A)@b
+    return x
