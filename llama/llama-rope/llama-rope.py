a6acc935ad3f6956e1d1d007054e6d46c995069c
Mehta Soham Priyadarshi <be24b030@smail.iitm.ac.in>
2026-06-15 23:44:27 +0530

    Solve llama-rope

--- llama/llama-rope/llama-rope.py
+++ llama/llama-rope/llama-rope.py
@@ -4,17 +4,17 @@ def apply_rope(q, k, freqs_cos, freqs_sin):
     """
     Returns: tuple of (q_rotated, k_rotated) same shapes as input
     """
-    q_even = q[..., 0::2]
-    q_odd = q[..., 1::2]
-    k_even = k[..., 0::2]
-    k_odd = k[..., 1::2]
+    # YOUR CODE HERE
+    q_x = q[..., 0::2]
+    q_y = q[..., 1::2]
+    k_x = k[..., 0::2]
+    k_y = k[..., 1::2]
+    q_stack = torch.stack((q_x,q_y),dim=0)*freqs_cos + torch.stack((-q_y,q_x),dim=0)*freqs_sin
     q_rotated = torch.zeros_like(q)
+    q_rotated[..., 0::2] = q_stack[0]
+    q_rotated[..., 1::2] = q_stack[1]
+    k_stack = torch.stack((k_x,k_y),dim=0)*freqs_cos + torch.stack((-k_y,k_x),dim=0)*freqs_sin
     k_rotated = torch.zeros_like(k)
-    cos = freqs_cos.unsqueeze(0).unsqueeze(0)
-    sin = freqs_sin.unsqueeze(0).unsqueeze(0)
-    q_rotated[..., 0::2] = q_even*cos - q_odd*sin
-    q_rotated[...,1::2] = q_even*sin + q_odd*cos
-    k_rotated[..., 0::2] = k_even*cos - k_odd*sin
-    k_rotated[...,1::2] = k_even*sin + k_odd*cos
-    
+    k_rotated[..., 0::2] = k_stack[0]
+    k_rotated[..., 1::2] = k_stack[1]
     return q_rotated, k_rotated
